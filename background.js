f2b63b1fbf3c32e2f8e4e84df7562dad7430b9c5
vycb <vycb777@gmail.com>
2019-10-07 12:51:42 +0300

    v2.1.5

--- background.js
+++ background.js
@@ -1,14 +1,14 @@
 function setUpMenu(request, sender, sendResponse){
-	if (request.cmd === "updateListRecentlyClosed" ){
-		console.log(`RecentlyClosed OnContextMenu cmd: ${request.cmd}`)
-	}
+	//if (request.cmd === "updateListRecentlyClosed" ){
+	//console.log(`RecentlyClosed OnContextMenu cmd: ${request.cmd}`)
+	//}
 
 	function listRecentClosed(sessionInfos){
 		chrome.contextMenus.removeAll() //{{{
 
 		let i=0
 		for (let sessionInfo of sessionInfos) {
-			let itemTitle, url, tab, sessionId
+			let itemTitle, url, tab, sessionId=null
 
 			if(sessionInfo.tab){
 				tab= sessionInfo.tab
@@ -20,6 +20,7 @@ function setUpMenu(request, sender, sendResponse){
 				itemTitle = 'w) '+ (tab.title? tab.title : tab.url)
 				sessionId = sessionInfo.window.sessionId
 			}
+			if(!sessionId)continue
 			url = new URL(tab.url)
 
 			//console.log(`sessionInfo.item: ${itemTitle}`)
@@ -29,25 +30,34 @@ function setUpMenu(request, sender, sendResponse){
 				itemTitle = beg+'...'+end
 			}
 
-			browser.menus.create({ id: ("recentlyclosed"+ i++ +"-" + sessionId), title: itemTitle, contexts: ["all"], enabled: true, onclick: (info, tab)=>{
+			browser.menus.create({ id: (("recentlyclosed"+ i++ +"-") + sessionId), title: itemTitle, contexts: ["all"], enabled: true/*, onclick: (info, tab)=>{
 				let idx = info.menuItemId.indexOf('-'),
 					sesId = info.menuItemId.slice(idx+1)
 
 				browser.sessions.restore(sesId)
 				
-			}
+			}*/
 				, icons: {16: url.origin+"/favicon.ico"}
 			})
 		}
 	} //}}}
 
-	var gettingSessions = browser.sessions.getRecentlyClosed()
+	var gettingSessions = browser.sessions.getRecentlyClosed({
+		maxResults: 25
+  })
 
 	gettingSessions.then(listRecentClosed, error=>{
 		console.log(error)
 	})
-
 }
+
+browser.menus.onClicked.addListener((info, tab)=>{
+	let idx = info.menuItemId.indexOf('-'),
+		sesId = info.menuItemId.slice(idx+1)
+
+	browser.sessions.restore(sesId)
+
+})
 
 browser.runtime.onMessage.addListener(setUpMenu)
 
